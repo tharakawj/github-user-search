1e2a720cbeb592cdbf2e27411f48efef342f2c3c
Tharaka Wijebandara <tharaka.nw@gmail.com>
2017-09-26 06:08:02 +0530

    Add basic react/redux folder structure to the project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import SearchPage from "./containers/SearchPage";
+import UserPage from "./containers/UserPage";
+import NotFoundPage from "./containers/NotFoundPage";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store} >
+        <Router>
+          <div>
+            <Switch>
+              <Route path="/" exact component={SearchPage}/>
+              <Route path="/user/:id" component={UserPage}/>
+              <Route component={NotFoundPage}/>
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ src/containers/NotFoundPage.js
@@ -0,0 +1,5 @@
+import React,  { Component } from "react";
+
+const NotFoundPage = () => <div>Not Found!</div>;
+
+export default NotFoundPage;
